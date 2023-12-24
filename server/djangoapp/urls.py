dad103767b53d36668da83cc7797f38c305ffefe
kurk <kurk.fisher@outlook.com>
2023-12-23 20:17:38 -0500

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-
+    path(route='about', view=views.about, name='About'),
     # path for contact us view
-
+    path(route='contact', view=views.contact, name='Contact'),
     # path for registration
 
     # path for login
